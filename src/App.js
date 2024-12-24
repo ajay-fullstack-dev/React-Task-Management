f859c32c514b143bf9f00ceca9a86848d0f8df3b
ajay modi <amajaymodi4@gmail.com>
2024-12-24 23:50:54 +0530

    Admin Create task ui is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
-
+import AdminDashboard from './components/Dashboard/AdminDashboard';
 const App = () => {
   return (
     <div className="App">
-     <EmployeeDashboard />
-
+     {/* <EmployeeDashboard /> */}
+      <AdminDashboard />
     </div>
   );
 }
