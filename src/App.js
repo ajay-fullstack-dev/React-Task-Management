821649a0cb7eb9d24669b5f0a994834ac751ce11
ajay modi <amajaymodi4@gmail.com>
2024-12-25 15:49:33 +0530

    worked on auth context and fake data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { useState, useContext } from 'react';
 import './App.css';
 import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import AdminDashboard from './components/Dashboard/AdminDashboard';
+import { AuthContext } from './context/AuthProvider';
+
 const App = () => {
+
+  const [user, setUser] = useState(null)
+  const AuthData = useContext(AuthContext)
+  const handleLogin = (email, password) => {
+    if(email === "admin@example.com" && password === "123456"){
+      setUser('admin');
+    }else if (email === "employee@example.com" && password === "123456") {
+      setUser('user')
+    }else{
+      alert('Invalid detail!');
+    }
+  }
+
+  
+  
+
   return (
     <div className="App">
-     {/* <EmployeeDashboard /> */}
-      <AdminDashboard />
+      {!user ? <Login handleLogin={handleLogin} /> : "" }
+      {user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard /> }
     </div>
   );
 }
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,13 +1,12 @@
 import React, {useState} from 'react';
 
-const Login = () => {
-
+const Login = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const { handleLogin } = props;
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(`form submitted with email: ${email} and password: ${password}`);
+    handleLogin(email,password)
     setEmail("");
     setPassword("");
   }
